525fb2c27e5b35369f025158930cfd75e5351add
alamin <157132743+alamin-codes@users.noreply.github.com>
2026-06-19 10:16:19 +0600

    Update AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,19 +1,18 @@
-import { Route, Routes } from "react-router"
-import Home from "../pages/Home/Home"
-import Cart from "../pages/Cart/Cart"
-import PlaceOrder from "../pages/PlaceOrder/PlaceOrder"
-import MyOrder from "../pages/MyOrders/MyOrders"
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home/Home";
+import Cart from "../pages/Cart/Cart";
+import PlaceOrder from "../pages/PlaceOrder/PlaceOrder";
+import MyOrder from "../pages/MyOrders/MyOrders";
 
 const AppRoutes = () => {
   return (
-    <Routes >
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/order" element={<PlaceOrder />} />
-        <Route path="/myorders" element={<MyOrder />} />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/order" element={<PlaceOrder />} />
+      <Route path="/myorders" element={<MyOrder />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
